c30f02b782c23ab99815cd1cd6e7b4bfa3a7cce2
javo2023 <jguzmanalarcon@gmail.com>
2023-07-26 20:21:31 -0400

    Commit nav ok

---
+++ app/src/main/java/com/example/ejercicio1_m6/AddInterface.kt
@@ -0,0 +1,4 @@
+package com.example.ejercicio1_m6
+
+interface AddInterface {
+}
--- app/src/main/java/com/example/ejercicio1_m6/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio1_m6/MainActivity.kt
@@ -2,6 +2,18 @@ package com.example.ejercicio1_m6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+/*
+[] Navigation
+[] Crear Fragmentos Agregar
+[] Crear Interfaz para agregar nueva tarea
+[]Room
+    []Dao
+    []BAse_datos
+    []Entity
+[] Crear Fragmentos Listado
+
+[]RecyclerView
+*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
