084ba73eaa13b2f94517dc797413fa2cf4637a7f
javo2023 <jguzmanalarcon@gmail.com>
2023-07-26 22:13:36 -0400

    Se implementa AddFragment

--- app/src/main/java/com/example/ejercicio1_m6/AddFragment.kt
+++ app/src/main/java/com/example/ejercicio1_m6/AddFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ejercicio1_m6.databinding.FragmentAddBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +20,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class AddFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+  lateinit var binding : FragmentAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        binding = FragmentAddBinding.inflate(layoutInflater,container,false)
+      initListener()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initListener() {
+        binding.buttonAddTask.setOnClickListener(){
+            val texto = binding.etAddTask.text.toString()
+            saveTask(texto)
+        }
     }
+
+    private fun saveTask(texto:String) {
+        val dao =TaskData.getDatabase(requireContext()).getTaskDao()
+        val task = Task(texto,"fecha")
+        GlobalScope.launch { dao.insertarTarea(task) }
+
+    }
+
+
 }
